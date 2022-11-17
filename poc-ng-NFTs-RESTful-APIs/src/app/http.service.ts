4324704cea37c767165a1bf803cb2b83741168d8
Borcze Angelov <Borcze.Angelov@yahoo.com>
2022-11-17 08:07:45 +0100

    update imports

--- poc-ng-NFTs-RESTful-APIs/src/app/http.service.ts
+++ poc-ng-NFTs-RESTful-APIs/src/app/http.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { NftAssets } from './dto/NftAssets';
+import { NftAssets } from './data/dto/NftAssets';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +10,6 @@ export class HttpService {
   public apiKey = ''; //at the moment is set by user during runtime
 
   constructor(private http: HttpClient) { }
-
 
   getRequest(url: string): Observable<NftAssets> {
     var fullUrl = url + '&apiKey=' + this.apiKey;
@@ -21,7 +20,6 @@ export class HttpService {
   getImage(url: string, mediaPath: string): Observable<any> {
     var fullUrl = url + mediaPath + '?apiKey=' + this.apiKey;
     console.log("getImage() fullUrl:" + fullUrl);
-    return this.http.get<any>(fullUrl,
-      { responseType: 'blob' as 'json' });
+    return this.http.get<any>(fullUrl, { responseType: 'blob' as 'json' });
   }
 }
