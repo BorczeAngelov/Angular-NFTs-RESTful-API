6a9e5b24971c2848c7b0e51ced2f8a9abab45fe2
Borcze Angelov <Borcze.Angelov@yahoo.com>
2022-11-18 08:10:44 +0100

    usability improvement

--- poc-ng-NFTs-RESTful-APIs/src/app/bored-ape-board/bored-ape-board.component.ts
+++ poc-ng-NFTs-RESTful-APIs/src/app/bored-ape-board/bored-ape-board.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { NftAssetWithMedia } from '../data/NftAssetWithMedia';
-import { HttpService } from '../http.service';
 import { NftMediaService } from '../nft-media/nft-media.service';
 
 @Component({
@@ -10,20 +9,23 @@ import { NftMediaService } from '../nft-media/nft-media.service';
   styleUrls: ['./bored-ape-board.component.css']
 })
 export class BoredApeBoardComponent implements OnInit {
-  private queryParameter = "assets?collection_name=Bored Ape Yacht Club";
+  private initialQueryParameter = "assets?collection_name=Bored Ape Yacht Club";
   private initialUrl = "https://svc.blockdaemon.com/nft/v1/ethereum/mainnet/"
-  public urlValue = this.initialUrl + this.queryParameter;
-  // public response$!: Observable<NftAssets>;
+
+  public urlValue = this.initialUrl;
+  public queryParameter = this.initialQueryParameter;
+  
   public response$!: Observable<NftAssetWithMedia[]>; //todo: refac to Observable<NftAssetWithMedia>
 
-  constructor(private httpService: HttpService, private nftMediaService: NftMediaService) { }
+  constructor(private nftMediaService: NftMediaService) { }
 
   ngOnInit(): void {
   }
 
   onSendRequest() {
     // this.response$ = this.httpService.getRequest(this.urlValue);
-    console.log("onSendRequest()" + this.urlValue);
+    console.log("onSendRequest() urlValue:" + this.urlValue);
+    console.log("onSendRequest() urlValue:" + this.queryParameter);
     this.response$ = this.nftMediaService.getAssetsWithMedia(this.initialUrl, this.queryParameter);
   }
 }
